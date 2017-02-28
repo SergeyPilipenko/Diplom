d1e052ed21aeec87fb67f19a3c6d87ef6ac9deb3
Admin <admin@Mac-Admin.local>
2017-02-28 16:22:18 +0300

    sdfd

---
+++ DiplomMagistratura/CinemaViewController.swift
@@ -0,0 +1,54 @@
+//
+//  CinemaViewController.swift
+//  DiplomMagistratura
+//
+//  Created by Admin on 28.02.17.
+//  Copyright © 2017 SergeyPilipenko. All rights reserved.
+//
+
+import UIKit
+
+class CinemaViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var headerlabel: UILabel!
+
+    
+    //MARK: - Variables
+    
+    var headerString: String? {
+        didSet{
+            configureView()
+        }
+    }
+    
+    //MARK: - Functions
+    
+    func configureView() {
+        headerlabel.text = headerString
+    }
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- DiplomMagistratura/MapViewController.swift
+++ DiplomMagistratura/MapViewController.swift
@@ -12,6 +12,16 @@ class MapViewController: UIViewController {
 
     @IBOutlet weak var webView: UIWebView!
     
+    let data = ["Spartak", "Proletka", "CinemaPark"]
+    
+    var views = [UIView]()
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    var snap: UISnapBehavior!
+    var previousTouchPoint: CGPoint!
+    var viewDragging = false
+    var viewPinned = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
       
@@ -24,11 +34,46 @@ class MapViewController: UIViewController {
           }else{
             print("not ok")
         }
+        
+        animator = UIDynamicAnimator(referenceView: self.view)
+        gravity = UIGravityBehavior()
+        
+        animator.addBehavior(gravity)
+        gravity.magnitude = 4
     
 
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    func addChildViewController(atOffset offset: CGFloat, dataForVC data: AnyObject?) ->UIView? {
+        let frameForView = self.view.bounds.offsetBy(dx: 0, dy: self.view.bounds.size.height - offset)
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let cinemaVC = sb.instantiateViewController(withIdentifier: "cinemaVC")
+        if let view = cinemaVC.view{
+            view.frame = frameForView
+            
+            if let headingStr = data as? String{
+                cinemaVC.headerString = headingStr
+            }
+            
+            self.addChildViewController(cinemaVC)
+            self.view.addSubview(view)
+            cinemaVC.didMove(toParentViewController: self)
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
