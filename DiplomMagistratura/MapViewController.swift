45a53316ab94c48a393e7e4550e5de7d67663867
Admin <admin@Mac-Admin.local>
2017-03-09 21:30:33 +0300

    Configure data in Realm

--- DiplomMagistratura/MapViewController.swift
+++ DiplomMagistratura/MapViewController.swift
@@ -31,37 +31,6 @@ class MapViewController: UIViewController, UICollectionViewDelegate, UICollectio
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let config = Realm.Configuration(
-            // Set the new schema version. This must be greater than the previously used
-            // version (if you've never set a schema version before, the version is 0).
-            schemaVersion: 1,
-            
-            // Set the block which will be called automatically when opening a Realm with
-            // a schema version lower than the one set above
-            migrationBlock: { migration, oldSchemaVersion in
-                // We haven’t migrated anything yet, so oldSchemaVersion == 0
-                if (oldSchemaVersion < 1) {
-                    // Nothing to do!
-                    // Realm will automatically detect new properties and removed properties
-                    // And will update the schema on disk automatically
-                }
-        })
-        
-        // Tell Realm to use this new configuration object for the default Realm
-        Realm.Configuration.defaultConfiguration = config
-        
-        // Now that we've told Realm how to handle the schema change, opening the file
-        // will automatically perform the migration
-        let realm = try! Realm()
-        
-       /* let spartak = Cinema()
-        spartak.name = "Спартак"
-        
-        try! realm.write {
-            realm.add(spartak)
-        }*/
-        
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
        
           if let path =  Bundle.main.path(forResource: "MapHTMLCode", ofType: "html") {
             print("ok")
