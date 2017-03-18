9c5acbe7cac48fe1c45c79ae133a98f674c98699
Admin <admin@Mac-Admin.local>
2017-03-18 17:53:00 +0300

    Added and start configure FilmsCollectionViewController

---
+++ DiplomMagistratura/FilmCollectionViewCell.swift
@@ -0,0 +1,100 @@
+//
+//  FilmCollectionViewCell.swift
+//  DiplomMagistratura
+//
+//  Created by Admin on 18.03.17.
+//  Copyright © 2017 SergeyPilipenko. All rights reserved.
+//
+
+import UIKit
+
+class FilmCollectionViewCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let thumbnailImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.blue
+        return imageView
+    }()
+    
+    let separatorView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        return view
+    }()
+    
+    let ratingLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1"
+        label.textAlignment = .center
+        label.layer.cornerRadius = 22
+        label.layer.borderColor = UIColor.red.cgColor
+        label.layer.borderWidth = 1
+        return label
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = .green
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let subtitleTextView: UITextView = {
+        let textView = UITextView()
+        textView.backgroundColor = .green
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    func setupViews(){
+        addSubview(thumbnailImageView)
+        addSubview(separatorView)
+        addSubview(ratingLabel)
+        addSubview(titleLabel)
+        addSubview(subtitleTextView)
+    
+        addConstraintsWithFormat(format: "H:|-16-[v0]-16-|", views: thumbnailImageView)
+        addConstraintsWithFormat(format: "V:|-16-[v0]-8-[v1(44)]-16-[v2(1)]|", views: thumbnailImageView, ratingLabel, separatorView)
+        addConstraintsWithFormat(format: "H:|-16-[v0(44)]", views: ratingLabel)
+       addConstraintsWithFormat(format: "H:|[v0]|", views: separatorView)
+       
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .top, relatedBy: .equal, toItem: thumbnailImageView, attribute: .bottom, multiplier: 1, constant: 8))
+        
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .left, relatedBy: .equal, toItem: ratingLabel, attribute: .right, multiplier: 1, constant: 8))
+        
+         addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
+        
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+      
+        addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .top, relatedBy: .equal, toItem: titleLabel, attribute: .bottom, multiplier: 1, constant: 8))
+        
+        addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .left, relatedBy: .equal, toItem: ratingLabel, attribute: .right, multiplier: 1, constant: 8))
+        
+        addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .right, relatedBy: .equal, toItem: thumbnailImageView, attribute: .right, multiplier: 1, constant: 0))
+        
+        addConstraint(NSLayoutConstraint(item: subtitleTextView, attribute: .height, relatedBy: .equal, toItem: self, attribute: .height, multiplier: 0, constant: 20))
+    }
+}
+
+extension UIView{
+    
+    func addConstraintsWithFormat(format: String, views: UIView...){
+        
+        var viewsDictionary = [String: UIView]()
+        for(index, view) in views.enumerated(){
+            let key = "v\(index)"
+            view.translatesAutoresizingMaskIntoConstraints = false
+            viewsDictionary[key] = view
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
+    }
+}
